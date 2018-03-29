fa641eaf0afaa5dc0b1e786ee33be589811a05c2
tayoung3 <36311169+tayoung3@users.noreply.github.com>
2018-03-29 14:44:38 -0400

    Add files via upload
    Receiver Python Script

---
+++ Source/RX.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: UHD RX DPSK
+# Generated: Thu Mar 29 14:42:39 2018
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import qtgui
+from gnuradio import uhd
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.qtgui import Range, RangeWidget
+from grc_gnuradio import blks2 as grc_blks2
+from optparse import OptionParser
+import sip
+import sys
+import time
+from gnuradio import qtgui
+
+
+class RX(gr.top_block, Qt.QWidget):
+
+    def __init__(self, address='addr=192.168.10.5', freq=2.45e9, freq_offset=0, gain=0, samp_rate=400e3):
+        gr.top_block.__init__(self, "UHD RX DPSK")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("UHD RX DPSK")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "RX")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.address = address
+        self.freq = freq
+        self.freq_offset = freq_offset
+        self.gain = gain
+        self.samp_rate = samp_rate
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.tun_gain = tun_gain = 0
+        self.tun_freq = tun_freq = freq
+        self.samps_per_sym = samps_per_sym = 4
+        self.rx_freq_off = rx_freq_off = 0
+        self.rolloff = rolloff = .35
+        self.nfilts = nfilts = 32
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self._tun_freq_range = Range(freq-0.05e9, freq+0.05e9, 1, freq, 200)
+        self._tun_freq_win = RangeWidget(self._tun_freq_range, self.set_tun_freq, 'UHD Freq (Hz)', "counter_slider", float)
+        self.top_layout.addWidget(self._tun_freq_win)
+        self._rx_freq_off_range = Range(-100e3, 100e3, 1, 0, 200)
+        self._rx_freq_off_win = RangeWidget(self._rx_freq_off_range, self.set_rx_freq_off, 'Rx Freq Offset (Hz)', "counter_slider", float)
+        self.top_layout.addWidget(self._rx_freq_off_win)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join((address, "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(tun_freq+rx_freq_off, 0)
+        self.uhd_usrp_source_0.set_gain(gain, 0)
+        self.uhd_usrp_source_0.set_antenna('RX2', 0)
+        self._tun_gain_range = Range(0, 20, 1, 0, 200)
+        self._tun_gain_win = RangeWidget(self._tun_gain_range, self.set_tun_gain, 'UHD Tx Gain', "counter_slider", float)
+        self.top_layout.addWidget(self._tun_gain_win)
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
+        	1024, #size
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	tun_freq, #fc
+        	samp_rate, #bw
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
+
+        if not False:
+          self.qtgui_freq_sink_x_0.disable_legend()
+
+        if "complex" == "float" or "complex" == "msg_float":
+          self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.digital_dxpsk_demod_0 = digital.dqpsk_demod(
+        	samples_per_symbol=samps_per_sym,
+        	excess_bw=0.35,
+        	freq_bw=6.28/100.0,
+        	phase_bw=6.28/100.0,
+        	timing_bw=6.28/100.0,
+        	mod_code="gray",
+        	verbose=False,
+        	log=False
+        )
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/home/eric/Desktop/out.txt', False)
+        self.blocks_file_sink_0.set_unbuffered(True)
+        self.blks2_packet_decoder_0 = grc_blks2.packet_demod_b(grc_blks2.packet_decoder(
+        		access_code='',
+        		threshold=-1,
+        		callback=lambda ok, payload: self.blks2_packet_decoder_0.recv_pkt(ok, payload),
+        	),
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blks2_packet_decoder_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.digital_dxpsk_demod_0, 0), (self.blks2_packet_decoder_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.digital_dxpsk_demod_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "RX")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_address(self):
+        return self.address
+
+    def set_address(self, address):
+        self.address = address
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.set_tun_freq(self.freq)
+
+    def get_freq_offset(self):
+        return self.freq_offset
+
+    def set_freq_offset(self, freq_offset):
+        self.freq_offset = freq_offset
+
+    def get_gain(self):
+        return self.gain
+
+    def set_gain(self, gain):
+        self.gain = gain
+        self.uhd_usrp_source_0.set_gain(self.gain, 0)
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.tun_freq, self.samp_rate)
+
+    def get_tun_gain(self):
+        return self.tun_gain
+
+    def set_tun_gain(self, tun_gain):
+        self.tun_gain = tun_gain
+
+    def get_tun_freq(self):
+        return self.tun_freq
+
+    def set_tun_freq(self, tun_freq):
+        self.tun_freq = tun_freq
+        self.uhd_usrp_source_0.set_center_freq(self.tun_freq+self.rx_freq_off, 0)
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.tun_freq, self.samp_rate)
+
+    def get_samps_per_sym(self):
+        return self.samps_per_sym
+
+    def set_samps_per_sym(self, samps_per_sym):
+        self.samps_per_sym = samps_per_sym
+
+    def get_rx_freq_off(self):
+        return self.rx_freq_off
+
+    def set_rx_freq_off(self, rx_freq_off):
+        self.rx_freq_off = rx_freq_off
+        self.uhd_usrp_source_0.set_center_freq(self.tun_freq+self.rx_freq_off, 0)
+
+    def get_rolloff(self):
+        return self.rolloff
+
+    def set_rolloff(self, rolloff):
+        self.rolloff = rolloff
+
+    def get_nfilts(self):
+        return self.nfilts
+
+    def set_nfilts(self, nfilts):
+        self.nfilts = nfilts
+
+
+def argument_parser():
+    parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
+    parser.add_option(
+        "-a", "--address", dest="address", type="string", default='addr=192.168.10.5',
+        help="Set IP Address [default=%default]")
+    parser.add_option(
+        "-f", "--freq", dest="freq", type="eng_float", default=eng_notation.num_to_str(2.45e9),
+        help="Set Default Frequency [default=%default]")
+    parser.add_option(
+        "-o", "--freq-offset", dest="freq_offset", type="eng_float", default=eng_notation.num_to_str(0),
+        help="Set Rx Frequency Offset [default=%default]")
+    parser.add_option(
+        "-g", "--gain", dest="gain", type="eng_float", default=eng_notation.num_to_str(0),
+        help="Set Default Gain [default=%default]")
+    parser.add_option(
+        "-s", "--samp-rate", dest="samp_rate", type="eng_float", default=eng_notation.num_to_str(400e3),
+        help="Set Sample Rate [default=%default]")
+    return parser
+
+
+def main(top_block_cls=RX, options=None):
+    if options is None:
+        options, _ = argument_parser().parse_args()
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls(address=options.address, freq=options.freq, freq_offset=options.freq_offset, gain=options.gain, samp_rate=options.samp_rate)
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
